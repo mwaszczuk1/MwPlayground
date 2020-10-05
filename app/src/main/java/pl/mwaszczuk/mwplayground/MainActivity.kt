5b6f67709399d51eda85093b108e6069bd569990
Marek.Waszczuk <1marek.waszczuk@gmail.com>
2020-10-05 08:37:00 +0200

    initial commit

---
+++ app/src/main/java/pl/mwaszczuk/mwplayground/MainActivity.kt
@@ -0,0 +1,18 @@
+package pl.mwaszczuk.mwplayground
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import pl.mwaszczuk.mwplayground.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/pl/mwaszczuk/mwplayground/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package pl.mwaszczuk.mwplayground.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
